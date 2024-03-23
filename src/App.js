d8f03f4bb6c50101cbf85ef20a086287650fa9b5
arindolghosh44 <arindolghosh.44@gmail.com>
2024-03-23 20:09:40 +0530

    ghgh90

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Footer from './customer/components/Footer/Footer'
 import Product from './customer/components/Product/Product';
 import ProductDetails from './customer/components/ProductDetails/ProductDetails';
 import Cart from './customer/components/Cart/Cart';
+import Checkout from './customer/components/Checkout/Checkout';
 
 
 function App() {
@@ -17,8 +18,9 @@ function App() {
 {/*<Product/>*/}
 
 {/*<ProductDetails/>*/}
-<Cart/>
-
+{/*
+<Cart/>*/}
+<Checkout/>
   </div>
   <Footer/>
     </div>
