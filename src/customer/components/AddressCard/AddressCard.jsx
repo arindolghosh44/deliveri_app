1e00d66320ca59c17c83a0636a51e92ec84d4a3a
arindolghosh44 <arindolghosh.44@gmail.com>
2024-03-24 10:57:08 +0530

    789

---
+++ src/customer/components/AddressCard/AddressCard.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const AddressCard = () => {
+  return (
+    <div>
+      <div className='space-y-3'>
+<p className='font-semibold'>
+Raam Kapoor
+
+</p>
+<p>Lorem ipsum dolor sit ame</p>
+<div className='space-y-1'>
+<p className='font-semibold'>Phone Number</p>
+<p>439045830303</p>
+</div>
+
+
+      </div>
+    </div>
+  )
+}
+
+export default AddressCard
--- src/customer/components/Checkout/Checkout.jsx
+++ src/customer/components/Checkout/Checkout.jsx
@@ -5,30 +5,24 @@ import Step from '@mui/material/Step';
 import StepLabel from '@mui/material/StepLabel';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import { useLocation } from 'react-router-dom';
+import DeliveryAddressForm from './DeliveryAddressForm';
+import OrderSummary from './OrderSummary';
 
-const steps = ['Select campaign settings', 'Create an ad group', 'Create an ad'];
+const steps = ['Login', ' Delivery Address', 'Order Summery','Payment'];
 
-export default function HorizontalLinearStepper() {
+export default function Checkout() {
   const [activeStep, setActiveStep] = React.useState(0);
-  const [skipped, setSkipped] = React.useState(new Set());
+ const location=useLocation();
+ const querySearch=new URLSearchParams(location.search);
+ const step=querySearch.get("step");
 
-  const isStepOptional = (step) => {
-    return step === 1;
-  };
-
-  const isStepSkipped = (step) => {
-    return skipped.has(step);
-  };
-
+ 
   const handleNext = () => {
-    let newSkipped = skipped;
-    if (isStepSkipped(activeStep)) {
-      newSkipped = new Set(newSkipped.values());
-      newSkipped.delete(activeStep);
-    }
+   
 
     setActiveStep((prevActiveStep) => prevActiveStep + 1);
-    setSkipped(newSkipped);
+   
   };
 
   const handleBack = () => {
@@ -39,54 +33,52 @@ export default function HorizontalLinearStepper() {
  
 
   return (
-    <Box sx={{ width: '100%' }}>
-      <Stepper activeStep={activeStep}>
-        {steps.map((label, index) => {
-          const stepProps = {};
-          const labelProps = {};
-          if (isStepOptional(index)) {
-            labelProps.optional = (
-              <Typography variant="caption">Optional</Typography>
-            );
-          }
-          if (isStepSkipped(index)) {
-            stepProps.completed = false;
-          }
-          return (
-            <Step key={label} {...stepProps}>
-              <StepLabel {...labelProps}>{label}</StepLabel>
-            </Step>
-          );
-        })}
-      </Stepper>
-      {activeStep === steps.length ? (
-        <React.Fragment>
-          <Typography sx={{ mt: 2, mb: 1 }}>
-            All steps completed - you&apos;re finished
-          </Typography>
+    <div className='px-10 lg:px-20 mt-8'> <Box sx={{ width: '100%' }}>
+    <Stepper activeStep={step}>
+      {steps.map((label, index) => {
+        const stepProps = {};
+        const labelProps = {};
+       
+        return (
+          <Step key={label} {...stepProps}>
+            <StepLabel {...labelProps}>{label}</StepLabel>
+          </Step>
+        );
+      })}
+    </Stepper>
+    {activeStep === steps.length ? (
+      <React.Fragment>
+        <Typography sx={{ mt: 2, mb: 1 }}>
+          All steps completed - you&apos;re finished
+        </Typography>
+       
+      </React.Fragment>
+    ) : (
+      <React.Fragment>
+      
+        <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
+          <Button
+            color="inherit"
+            disabled={activeStep === 0}
+            onClick={handleBack}
+            sx={{ mr: 1 }}
+          >
+            Back
+          </Button>
          
-        </React.Fragment>
-      ) : (
-        <React.Fragment>
-          <Typography sx={{ mt: 2, mb: 1 }}>Step {activeStep + 1}</Typography>
-          <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
-            <Button
-              color="inherit"
-              disabled={activeStep === 0}
-              onClick={handleBack}
-              sx={{ mr: 1 }}
-            >
-              Back
-            </Button>
-            <Box sx={{ flex: '1 1 auto' }} />
-           
+        </Box>
 
-            <Button onClick={handleNext}>
-              {activeStep === steps.length - 1 ? 'Finish' : 'Next'}
-            </Button>
-          </Box>
-        </React.Fragment>
-      )}
-    </Box>
+
+<div>
+  {step==2?<DeliveryAddressForm/>:<OrderSummary/>}
+</div>
+
+
+
+
+      </React.Fragment>
+    )}
+  </Box></div>
+   
   );
 }
